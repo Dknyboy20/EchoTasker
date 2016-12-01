22b4fa28804bd7047ff7eb37a7a6a45e4326cf0a
Jason Ratliff <jratliff681@gmail.com>
2016-12-01 14:41:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -27,9 +27,12 @@ EchoTasker.prototype.intentHandlers = {
 
     TaskerIntent: function (intent, session, response) {
         console.log("TaskerIntent received");
+        // if using https below make sure to change the variable in the options.js file "usehttps" to true
         httpreq('http://autoremotejoaomgcd.appspot.com/sendmessage?key=' + options.ARkey + '&message=echo=:=' + intent.slots.Message.value, function(error) {
-            response.tell("Auto Remote message sent");
-//            genericResponse(error, response);
+            response.tell("Auto Remote message sent"); // This sends the message back to the Echo to speak.
+
+//            var speech = "Auto Remote message sent";  // Can also do it this way instead of response.tell above, if there was an error
+//            genericResponse(error, response, speech); // in the http request it will speak the error otherwise speech variable will be spoken.
         });
     },
 
@@ -42,41 +45,24 @@ EchoTasker.prototype.intentHandlers = {
 }
 
 
-function httpreq(options, responseCallback) {
-    var transport = options.useHttps ? http : http;
-
-    var req = transport.request(options, function(httpResponse) {
+function httpreq(address, responseCallback) {
+    var transport = options.useHttps ? https : http;
+    var req = transport.request(address, function(httpResponse) {
         var body = '';
-
-        httpResponse.on('data', function(data) {
-            body += data;
-        });
-
-        httpResponse.on('end', function() {
-            responseCallback(undefined, body);
-        });
+        httpResponse.on('data', function(data) { body += data; });
+        httpResponse.on('end', function() { responseCallback(undefined, body); });
     });
-
-    req.on('error', function(e) {
-        responseCallback(e);
-    });
-
+    req.on('error', function(e) { responseCallback(e); });
     req.end();
 }
 
 
 function genericResponse(error, response, success) {
     if (!error) {
-        if (!success) {
-            response.tell("OK");
-        }
-        else {
-            response.tell(success);
-        }
+        if (!success) { response.tell("OK"); }
+        else { response.tell(success); }
     }
-    else {
-        response.tell("The Lambda service encountered an error: " + error.message);
-    }
+    else { response.tell("The Lambda service encountered an error: " + error.message); }
 }
 
 // Create the handler that responds to the Alexa Request.
